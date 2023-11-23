075fa9f35eec6f79173ed85906fd9b30fae3ef82
Dasha Musienko <60947524+dasha-musienko@users.noreply.github.com>
2023-11-23 20:23:12 +0200

    adds files

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,5 +1,4 @@
 import { createSelector } from '@reduxjs/toolkit';
-
 export const selectContacts = state => state.contacts.items;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
